79dca66998cf94a16012b153adf90dd445544417
Jared Lash <jared.c.lash@gmail.com>
2026-08-23 19:43:44 -0500

    Built out data models

---
+++ JobApplicationHelper/Data/Entities/ExperienceEvidenceEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace JobApplicationHelper.Data.Entities;
+
+public sealed class ExperienceEvidenceEntity
+{
+    public int Id { get; set; }
+
+    public string ExperienceId { get; set; } = string.Empty;
+
+    public int SortOrder { get; set; }
+
+    public string Text { get; set; } = string.Empty;
+
+    public ExperienceEntity Experience { get; set; } = null!;
+}
