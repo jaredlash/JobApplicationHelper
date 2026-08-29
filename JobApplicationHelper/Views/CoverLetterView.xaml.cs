3a2d3f3ca996d285dc82dc61c7ec6096b29e9ccb
Jared Lash <jared.c.lash@gmail.com>
2026-08-28 23:47:14 -0500

    split DraftWindow into multiple views

---
+++ JobApplicationHelper/Views/CoverLetterView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JobApplicationHelper.Views
+{
+    /// <summary>
+    /// Interaction logic for CoverLetterView.xaml
+    /// </summary>
+    public partial class CoverLetterView : UserControl
+    {
+        public CoverLetterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
