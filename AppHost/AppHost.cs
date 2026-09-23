5756fc30c306837bbdef4a96ab5f6c95b00cc302
Jared Lash <jared.c.lash@gmail.com>
2026-09-23 18:09:31 -0500

    Fixed broken reference after api project renaming

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -1,5 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Api>("api");
+builder.AddProject<Projects.JobApplicationHelper_Api>("api");
 
 builder.Build().Run();
